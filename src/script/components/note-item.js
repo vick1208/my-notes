96f0aece35eb4889bf3b034e74594a0b5b2fa554
Vicky AS <vickyhuang808@gmail.com>
2024-07-15 13:38:35 +0700

    komponen daftar note

---
+++ src/script/components/note-item.js
@@ -0,0 +1,3 @@
+class NoteItem extends HTMLElement{
+    
+}
--- src/script/views/home.js
+++ src/script/views/home.js
@@ -1,9 +1,11 @@
-
+// import Utils from '../utils.js'
 
 
 
 function home() {
-    
+    // const noteListContainerEl = document.querySelector('#noteListContainer');
+    // const noteListEl = noteListContainerEl.querySelector('note-list');
+
 }
 
 export default home;
