43562ed83388dd383491c8ae4ee7c4c325f4dba8
Vicky AS <vickyhuang808@gmail.com>
2024-07-15 14:52:30 +0700

    edit komponen note

--- src/script/components/note-list.js
+++ src/script/components/note-list.js
@@ -1,3 +1,43 @@
-class NoteList extends HTMLElement{
+class NoteList extends HTMLElement {
+    _shadowRoot = null;
+    _style = null;
 
+    constructor() {
+        super();
+
+        this._shadowRoot = this.attachShadow({ mode: "open" });
+        this._style = document.createElement('style');
+    }
+
+    _emptyContent() {
+        this._shadowRoot.innerHTML = '';
+    }
+
+    _updateStyle() {
+        this._style.textContent = `
+        
+        :host{
+        
+        
+        
+        }
+        
+
+        `;
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this._emptyContent();
+        this._updateStyle();
+        this._shadowRoot.appendChild(this._style);
+        this._shadowRoot.innerHTML = ``;
+
+    }
 }
+
+
+customElements.define('note-list',NoteList);
