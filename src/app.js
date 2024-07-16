fd28bacbecdd977a624ea1ef3863005562725c25
Vicky AS <vickyhuang808@gmail.com>
2024-07-16 23:28:22 +0700

    test ulang

--- src/app.js
+++ src/app.js
@@ -6,7 +6,85 @@ import home from './script/views/home.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     home();
+    loadNoteData();
+    setNoteFormListener();
 
 });
 
 
+const form = document.querySelector('.notes-form');
+const titleInput = form.elements.noteTitle;
+const bodyInput = form.elements.noteBody;
+
+form.addEventListener('submit', (event) => event.preventDefault());
+
+titleInput.addEventListener('invalid', (event) => {
+    event.target.setCustomValidity('');
+
+    if (!event.target.validity.valid) {
+        event.target.setCustomValidity('Wajib diisi.');
+        return;
+    };
+});
+
+bodyInput.addEventListener('invalid', (event) => {
+    event.target.setCustomValidity('');
+
+    if (!event.target.validity.valid) {
+        event.target.setCustomValidity('Wajib diisi.');
+        return;
+    };
+});
+
+
+function setNoteFormListener() {
+    const notesForm = document.getElementById('notesForm');
+
+    notesForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const noteTitle = document.getElementById('noteTitle').value;
+        const noteDesc = document.getElementById('noteBody').value;
+        const note = {
+            id: generateUniqueId(),
+            title: noteTitle,
+            body: noteDesc,
+            createdAt: new Date().toISOString(),
+            archived: false,
+        }
+
+        let notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+        notes.push(note);
+
+        localStorage.setItem('notes', JSON.stringify(notes))
+
+        this.reset();
+
+        loadNoteData();
+    });
+}
+
+function generateUniqueId() {
+    const timestamp = Date.now().toString();
+    const randomString = Math.random().toString(36).substring(2, 8);
+
+    return `notes-${randomString}-${timestamp}`;
+}
+
+
+function loadNoteData() {
+    const noteListElement = document.querySelector('note-list');
+    const notes = JSON.parse(localStorage.getItem('notes')) || [];
+
+    notes.forEach((note) => {
+        const noteItemElement = document.createElement('note-item');
+        noteItemElement.note = note;
+        noteListElement.append(noteItemElement);
+    });
+}
+
+const saveBtn = document.getElementById('saveBtn');
+saveBtn.addEventListener('click', function () { 
+    location.reload(); 
+});
--- src/script/views/home.js
+++ src/script/views/home.js
@@ -5,36 +5,12 @@ import NotesData from '../data/local/notesData.js'
 function home() {
     const noteListContainerElement = document.querySelector('#noteListContainer');
     const noteListElement = noteListContainerElement.querySelector('note-list');
-    const form = document.querySelector('.notes-form');
-    const titleInput = form.elements.noteTitle;
-    const bodyInput = form.elements.noteBody;
-    const saveBtn = document.getElementById('saveBtn');
-
-    form.addEventListener('submit', (event) => event.preventDefault());
-
-    titleInput.addEventListener('invalid', (event) => {
-        event.target.setCustomValidity('');
-
-        if (!event.target.validity.valid) {
-            event.target.setCustomValidity('Wajib diisi.');
-            return;
-        };
-    });
-
-    bodyInput.addEventListener('invalid',(event) => {
-        event.target.setCustomValidity('');
-
-        if (!event.target.validity.valid) {
-            event.target.setCustomValidity('Wajib diisi.');
-            return;
-        };
-    });
 
     const showPersonalNote = () => {
         const result = NotesData.getAll();
         displayResult(result);
-
-        // showNoteList();
+        
+        showNoteList();
     }
 
     function displayResult(notes) {
@@ -48,68 +24,15 @@ function home() {
         noteListElement.append(...noteItemElements);
     }
 
-    // function showNoteList() {
-    //     Array.from(noteListContainerElement.children).forEach((element) => {
-    //         Utils.hideElement(element);
-    //     });
-    //     Utils.showElement(noteListElement);
-    // }
-
-
-
+    function showNoteList() {
+        Array.from(noteListContainerElement.children).forEach((element) => {
+            Utils.hideElement(element);
+        });
+        Utils.showElement(noteListElement);
+    }
     showPersonalNote();
-
-    
-
-    
 }
 
-// function setNoteFormListener() {
-//     const notesForm = document.getElementById('notesForm');
 
-//     notesForm.addEventListener('submit', (event) => {
-//         event.preventDefault();
-
-//         const noteTitle = document.getElementById('noteTitle').value;
-//         const noteDesc = document.getElementById('noteBody').value;
-
-//         const note = {
-//             id: generateUniqueId(),
-//             title: noteTitle,
-//             body: noteDesc,
-//             createdAt: new Date().toISOString(),
-//             archived: false,
-//         }
-
-//         let notes = JSON.parse(localStorage.getItem('notes')) || [];
-
-//         notes.push(note);
-
-//         localStorage.setItem('notes', JSON.stringify(notes))
-
-//         this.reset();
-
-//         loadNoteData();
-//     });
-// }
-
-// function generateUniqueId() {
-//     const timestamp = Date.now().toString();
-//     const randomString = Math.random().toString(36).substring(2, 8);
-
-//     return `notes-${randomString}-${timestamp}`;
-// }
-
-
-// function loadNoteData() {
-//     const noteListElement = document.querySelector('note-list');
-//     const notes = JSON.parse(localStorage.getItem('notes')) || [];
-
-//     notes.forEach((note) => {
-//         const noteItemElement = document.createElement('note-item');
-//         noteItemElement.note = note;
-//         noteListElement.append(noteItemElement);
-//     });
-// }
 
 export default home;
