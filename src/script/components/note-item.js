3d247de2f11a3cdc42ff086e9d331709ccbfdb1b
Vicky AS <vickyhuang808@gmail.com>
2024-08-07 20:42:32 +0700

    change styling

--- src/script/components/note-item.js
+++ src/script/components/note-item.js
@@ -42,7 +42,7 @@ class NoteItem extends HTMLElement {
             height: 150px;
             padding: 50px;
             border-radius: 12px;
-            box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.5);
+            box-shadow: 0 3px 3px 2px rgba(0, 0, 0, 0.2);
             overflow: hidden;
 
         }
