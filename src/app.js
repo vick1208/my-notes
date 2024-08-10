b9f62b140c46da41453dabd88800d6ceb71a5c94
Vicky AS <vickyhuang808@gmail.com>
2024-08-10 11:12:36 +0700

    add aos library

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import "./script/components/index.js";
 import "./styles/style.css";
-
+import 'aos/dist/aos.css';
 import home from "./script/views/home.js";
+import AOS from "aos";
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  AOS.init();
+
   home();
 });
